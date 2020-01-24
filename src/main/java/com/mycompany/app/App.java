d318858a03a0be6743d636657855b85cf82cd18f
csreedhar20 <csreedhar20@gmail.com>
2020-01-23 20:12:21 -0600

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,15 +7,24 @@ public class App
 {
 
     private final String message = "GITHUBESSENTIALS:1234:547456:test@linux.com";
-
+    private final String message1 = "GITHUBESSENTIALS:9568:21547:test@linux.com";
+    private final String message2 = "GITSENTIALS:956800:21547000:test@linux.com";
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage1());
+        System.out.println(new App().getMessage2());
     }
 
     private final String getMessage() {
         return message;
+    }
+    private final String getMessage1() {
+        return message1;
+    }
+    private final String getMessage2() {
+        return message2;
     }
 
 }
